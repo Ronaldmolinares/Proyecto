d2bcef7ffe3dc492553fe95a1a823ebad16b5e0f
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-04-11 16:25:34 -0500

    Creando la clase Doctor

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,5 +1,25 @@
 package model;
 
 public class Doctor extends Person{
+    private String specialty;
+    
+    public Doctor (){
 
+    }
+
+    public Doctor(int id, String name, String lastName, String phone, String address, String email, String specialty) {
+        super(id, name, lastName, phone, address, email);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+
+    
 }
