bddb9c7f8bc857b751f110d850f0f6d260f30b29
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-04-11 15:41:44 -0500

    Creando la clase Person

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,5 +1,79 @@
 package model;
 
 public class Person {
+    private int id;
+    private String name;
+    private String lastName;
+    private String phone;
+    private String address;
+    private String email;
 
+    public Person (){
+        
+    }
+
+    public Person(int id, String name, String lastName, String phone, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + ", lastName=" + lastName + ", phone=" + phone + ", address="
+                + address + ", email=" + email + "]";
+    }
+
+    
 }
