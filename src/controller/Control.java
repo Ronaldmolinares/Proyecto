8a3827c8b200ebcc7878c570482ffe800ca54bab
Lunna SoSa <lunna.sosa@uptc.edu.co>
2023-04-12 10:31:09 -0500

    update control, run and view

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -1,5 +1,62 @@
 package controller;
 
+import view.IoManager;
+
 public class Control {
+    IoManager io;
+
+    public Control() {
+        io = new IoManager();
+    }
+
+    public void init() {
+        int opcion = 0;
+        do {
+            try {
+                opcion = io.readMenu();
+                switch (opcion) {
+                    case 1:
+                        this.addMedicalPractice();
+                        break;
+                    case 2:
+                        this.addDoctor();
+                        break;
+                    case 3:
+                        this.addPacient();
+                        break;
+                    case 4:
+                        this.generateBillPacient();
+                        break;
+                    case 5:
+                        this.generateBillMedicalPractice();
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        io.showGraphicMessage("You have selected an invalid option!");
+                        break;
+                }
+            } catch (NumberFormatException e) {
+                io.showGraphicErrorMessage("You must enter an integer");
+                io.showGraphicErrorMessage(e.getMessage());
+            }
+        } while (opcion != 6);
+        io.showGraphicMessage("See you later");
+    }
+
+    private void addMedicalPractice() {
+    }
+
+    private void addDoctor() {
+    }
+
+    private void addPacient() {
+    }
+
+    private void generateBillMedicalPractice() {
+    }
+
+    private void generateBillPacient() {
+    }
 
 }
