01e6bf0f774290a8e944175a0132719083c4167c
Lunna SoSa <lunna.sosa@uptc.edu.co>
2023-04-14 12:00:00 -0500

    update enum von alguos tratamientos

--- src/model/TreatmentEnum.java
+++ src/model/TreatmentEnum.java
@@ -1,5 +1,15 @@
 package model;
 
 public enum TreatmentEnum {
-    
+    GENERAL_CONSULT(50000),
+    ORTODONTIC_CONTROL(70000),
+    ORAL_SURGERY(120000),
+    PERIDONTICS(450000);
+
+    private double amount;
+
+    private TreatmentEnum(double amount) {
+        this.amount = amount;
+    }
+
 }
